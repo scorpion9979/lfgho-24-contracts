c2f92cacfce69e151abd26a5be3bfca7151dbc0c
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2024-01-21 08:01:07 -0400

    feat: more details for Sablier_CDP_Facilitator

--- contracts/Sablier_CDP_Facilitator.sol
+++ contracts/Sablier_CDP_Facilitator.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.19;
 import { ISablierV2Lockup } from "@sablier/v2-core/src/interfaces/ISablierV2Lockup.sol";
 import { SablierNFTPriceEngine } from "./SablierNFTPriceEngine.sol";
 import { Types } from "./libraries/Types.sol";
+import { GhoToken } from "gho-core/gho/GhoToken.sol";
 
 contract Sablier_CDP_Facilitator {
     /// @notice The maximum number of Sablier NFTs that can be deposited as collateral per user.
@@ -12,11 +13,18 @@ contract Sablier_CDP_Facilitator {
     /// @notice The Sablier NFT price engine.
     SablierNFTPriceEngine public immutable sablierNFTPriceEngine;
 
+    /// @notice The GHO token.
+    GhoToken public immutable gho;
+
     /// @notice Keeps track of the Sablier NFTs deposited by each user as collateral.
     mapping(address user => Types.SablierNFT[] nfts) public collateral;
 
-    constructor(SablierNFTPriceEngine _sablierNFTPriceEngine) {
+    /// @notice Keeps track of the GHO debt owed by each user.
+    mapping(address user => uint256) public debt;
+
+    constructor(SablierNFTPriceEngine _sablierNFTPriceEngine, GhoToken _gho) {
         sablierNFTPriceEngine = _sablierNFTPriceEngine;
+        gho = _gho;
     }
 
     /// @notice Deposit Sablier NFTs into the contract to be used as collateral.
@@ -39,29 +47,51 @@ contract Sablier_CDP_Facilitator {
     function borrow(uint256 amount) external {
         // Check that the user has sufficient collateral value to borrow `amount` of GHO.
         uint256 collateralValue = sablierNFTPriceEngine.getNormalizedValueAggregate(collateral[msg.sender]);
+        require(collateralValue >= amount, "Insufficient collateral value");
 
-        // TODO: mint `amount` of GHO to the user.
+        // Mint `amount` of GHO to the user.
+        gho.mint(msg.sender, amount);
     }
 
     /// @notice Repay GHO debt.
     /// @param amount The amount of GHO to be repaid.
     function repay(uint256 amount) external {
-        // TODO: check that the user has sufficient GHO balance to repay `amount` of GHO.
-        // TODO: check that `amount` of GHO is less than or equal to the user's debt.
-        // TODO: burn `amount` of GHO from the user.
+        // Check that the user has sufficient GHO balance to repay `amount` of GHO.
+        gho.transferFrom(msg.sender, address(this), amount);
+
+        // Check that `amount` of GHO is less than or equal to the user's debt.
+        require(amount <= debt[msg.sender], "Repaying too much");
+
+        // Burn `amount` of GHO from the user.
+        gho.burn(amount);
+
+        // Update the user's debt.
+        debt[msg.sender] -= amount;
     }
 
     /// @notice Withdraw Sablier NFTs from the contract that were used as collateral.
     /// @param idLength The number of Sablier NFTs to be withdrawn.
     function withdraw(uint256 idLength) external {
-        // TODO: check that the NFT(s) to be withdrawn all exist in the contract.
-        // TODO: check that the NFT(s) to be withdrawn were originally deposited by the user.
-        // TODO: check that the value of the Sablier NFTs is greater than the debt owed by the user.
+        // Check that the value of the Sablier NFTs is greater than the debt owed by the user.
+        Types.SablierNFT[] memory collateralToWithdraw = new Types.SablierNFT[](idLength);
         for (uint256 i = 0; i < idLength; i++) {
-            // Effect: transfer the NFT from the contract to the user.
-            collateral[msg.sender][i].sablier.transferFrom(address(this), msg.sender, collateral[msg.sender][i].id);
-            // Effect: update the collateral bookkeeping.
+            // Push collateral in reverse order.
+            collateralToWithdraw[i] = collateral[msg.sender][collateral[msg.sender].length - i - 1];
+        }
+        // Get the value of the Sablier NFTs to be withdrawn.
+        uint256 collateralValueToWithdraw = sablierNFTPriceEngine.getNormalizedValueAggregate(collateralToWithdraw);
+
+        // Compute new collateral value after withdrawal.
+        uint256 newCollateralValue =
+            sablierNFTPriceEngine.getNormalizedValueAggregate(collateral[msg.sender]) - collateralValueToWithdraw;
+
+        // Check that the new collateral value is greater than or equal to the debt owed by the user.
+        require(newCollateralValue >= debt[msg.sender], "Insufficient collateral value");
+
+        // Transfer the Sablier NFTs from the contract to the user.
+        for (uint256 i = 0; i < idLength; i++) {
             collateral[msg.sender].pop();
+            collateralToWithdraw[i].sablier.transferFrom(address(this), msg.sender, collateralToWithdraw[i].id);
         }
     }
 }
